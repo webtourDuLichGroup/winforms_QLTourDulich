601500771f62ba1800da6169c4c7eab880fdc89b
TranKhats <35250985+TranKhats@users.noreply.github.com>
2018-09-26 13:08:45 +0700

    1

--- Frm_DangNhap/Frm_DangNhap/Program.cs
+++ Frm_DangNhap/Frm_DangNhap/Program.cs
@@ -16,7 +16,7 @@ namespace Frm_DangNhap
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmFindServer());
         }
     }
 }
